88db7705c5ee2f8b05120ca8ba24affc03dc6395
Matt Dionis <mattdionis@gmail.com>
2016-11-25 19:32:19 -0500

    Add and configure socket.io

--- node_server/server.js
+++ node_server/server.js
@@ -1,27 +1,36 @@
-const express = require('express');
-const path = require('path');
-const twit = require('twitter');
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var twit = require('twitter');
 
-const app = express();
-const router = express.Router();
-const staticRoot = __dirname;
-const twitter = new twit({
+var app = express();
+var port = process.env.PORT || 3000;
+var router = express.Router();
+var staticRoot = __dirname;
+var twitter = new twit({
   consumer_key: TWITTER_CONSUMER_KEY,
   consumer_secret: TWITTER_CONSUMER_SECRET,
   access_token_key: TWITTER_ACCESS_TOKEN_KEY,
   access_token_secret: TWITTER_ACCESS_TOKEN_SECRET
 });
 
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (port));
 app.use(express.static(staticRoot));
 
 app.get('/', function(req, res) {
   res.sendFile('index.html');
 });
 
+var server = http.createServer(app).listen(port, function() {
+  console.log('Server listening on port ' + port);
+});
+
+var io = require('socket.io').listen(server);
+
 app.get('/stream', function(req, res, next) {
   twitter.stream('statuses/filter', {track: '#javascript'}, function(stream) {
     stream.on('data', function(tweet) {
+      io.emit('tweet', tweet);
       console.log(tweet.text);
     });
 
@@ -29,8 +38,4 @@ app.get('/stream', function(req, res, next) {
       throw error;
     });
   });
-});
-
-app.listen(app.get('port'), function() {
-  console.log('app running on port', app.get('port'));
 });
